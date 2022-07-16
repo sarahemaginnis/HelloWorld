20d22a105ddb560fa71075b03207f18f981086e5
sarahemaginnis <sarahemaginnis@gmail.com>
2022-07-16 11:18:40 -0500

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var animals = new string[] { "Triceratops", "Gorilla", "Corgi", "Toucan" };
+
+foreach (var animal in animals)
+{
+	Console.WriteLine(animal);
+}
+Console.Write("Type a letter: ");
+var input = Console.ReadLine();
+Console.WriteLine(input);
+Console.ReadKey(); // What happens if you run the app without this line?
